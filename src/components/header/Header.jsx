fb6288986917e50ec31d18d295a128ccca007f36
Twenfyx <senchik549@gmail.com>
2023-11-01 16:54:18 +0100

    change header layout

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import styles from './Header.module.css';
-
+import logo from "../../images/logo/logo.png"
 function Header() {
     return (
-        <div className={styles}>
-            <div className={styles.positions}>
-                <h1>Home</h1>
-                <h1>About us</h1>
-                <h1>Pricing</h1>
-                <h1>Contact</h1>
-                <h1>Join us</h1>
-            </div>
+            <div className={styles.main}>
+                <div className={styles.logo}>
+                <h1 className={styles.text}>Logo</h1>
+                {/* <img src={logo} alt="" /> */}
+                    </div>
+                <div className={styles.navigation}>
+                <h1 className={styles.text}>Home</h1>
+                <h1 className={styles.text}>About us</h1>
+                <h1 className={styles.text}>Pricing</h1>
+                <h1 className={styles.text}>Contact</h1>
+                </div>
+                <div className={styles.join}>
+                <h1 className={styles.text }>Sign in</h1>
+                </div>
         </div>
     );
 }
